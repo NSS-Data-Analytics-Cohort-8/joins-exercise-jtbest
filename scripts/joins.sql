8a492e693fefea55c63582af210b1ff587fe2e31
Jtbest <jtbest713@gmail.com>
2023-02-14 18:37:07 -0600

    fix

--- scripts/joins.sql
+++ scripts/joins.sql
@@ -78,7 +78,7 @@ LIMIT 1;
 
 SELECT d.company_name, COUNT(s.film_title) as total_movies
 FROM distributors as d
-INNER JOIN specs as s
+LEFT JOIN specs as s
 ON d.distributor_id = s.domestic_distributor_id
 GROUP BY d.company_name
 ORDER BY total_movies DESC;
@@ -87,7 +87,7 @@ ORDER BY total_movies DESC;
 
 -- 5. Write a query that returns the five distributors with the highest average movie budget.
 
-SELECT d.company_name, ROUND(AVG(r.film_budget),1) as avg_budget
+SELECT d.company_name, ROUND(AVG(r.film_budget),-3) as avg_budget
 FROM distributors as d
 INNER JOIN specs as s
 ON d.distributor_id = s.domestic_distributor_id
@@ -116,10 +116,10 @@ ON d.distributor_id = s.domestic_distributor_id
 INNER JOIN rating as r
 ON s.movie_id = r.movie_id
 WHERE d.headquarters NOT LIKE 'CA'
-ORDER BY r.imdb_rating
+ORDER BY r.imdb_rating DESC
 LIMIT 1;
 
---	Jaws 3-D
+--	The Dark Knight
 
 -- 7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours?
 
